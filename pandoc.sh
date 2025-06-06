87a6f915040179b3ed124f157917fd25473b493d
Don Badrak <dbadrak@gmail.com>
2025-06-06 19:08:30 -0400

    add timers.pdf

--- pandoc.sh
+++ pandoc.sh
@@ -1,2 +1,3 @@
 /usr/bin/pandoc -f gfm -t pdf officials.md -o officials.pdf
 /usr/bin/pandoc -f gfm -t pdf officials.no-image.md -o officials.no-image.pdf
+/usr/bin/pandoc -f gfm -t pdf timers.md -o timers.pdf
